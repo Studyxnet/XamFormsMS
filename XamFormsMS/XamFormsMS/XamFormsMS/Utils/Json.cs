813ced81b27324b70f16cf37e3450846d0a5d892
Rafael Moura <contato@rm.eti.br>
2015-12-07 10:24:00 -0200

    Json e WebCliente

---
+++ XamFormsMS/XamFormsMS/XamFormsMS/Utils/Json.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace VeraCruzMobile.Business
+{
+	public class JsonHelper <T> where T : class
+	{
+		public static string ObjectToJson(T ObjectToConvert)
+		{
+			JsonSerializerSettings _jsonWriter = new JsonSerializerSettings
+			{
+				NullValueHandling = NullValueHandling.Ignore
+			};
+
+			return JsonConvert.SerializeObject(ObjectToConvert, _jsonWriter);
+		}
+
+		public static T JsonToObject(string jsonText)
+		{
+			var retorno = JsonConvert.DeserializeObject<T>(jsonText);
+
+			return retorno;
+		}
+	}
+
+
+
+
+}
+
---
+++ XamFormsMS/XamFormsMS/XamFormsMS/Utils/WebService.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using System.Collections.Generic;
+
+namespace VeraCruzMobile.Business
+{
+	public class WebServiceBase <T> where T : class
+	{
+		private static string BaseUrl = "http://developers.agenciaideias.com.br/tempo/json/{0}-{1}";
+
+
+		public WebServiceBase ()
+		{
+		}
+
+
+		public static async Task<T> RequestAsync (string cidade, string uf, RequestType requestType = RequestType.Get, T SendObject = null, 
+		                                          int triesNumber = 0, string contentType = "application/json")
+		{
+
+			T tReturn = null;
+
+			for (int i = 0; i <= triesNumber; i++) {
+				try {
+
+					using (HttpClient client = new HttpClient ()) {
+
+						HttpResponseMessage response = new HttpResponseMessage ();
+
+						var requestBody = JsonHelper<T>.ObjectToJson (SendObject);
+
+						client.BaseAddress = new Uri (BaseUrl);
+
+						HttpContent httpContent = new StringContent (requestBody, Encoding.UTF8, contentType);
+
+
+						response = await client.GetAsync (String.Format (BaseUrl, cidade, uf));
+						break;
+						
+
+						if (response.IsSuccessStatusCode) {
+							string responseString = await response.Content.ReadAsStringAsync ();
+
+							var objetoRetorno = JsonHelper<T>.JsonToObject (responseString);
+
+
+							tReturn = objetoRetorno;
+
+						} else {
+							if (response.StatusCode == System.Net.HttpStatusCode.RequestTimeout) {
+								if (i == triesNumber)
+									throw  new Exception ("Tempo limite atingido");
+							} else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+								throw  new Exception ("Serviço não encontrado");
+							else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError) {
+								if (i == triesNumber)
+									throw new Exception ("Serviço indisponível no momento");
+							} else
+								throw new Exception ("Falha no acesso!");
+						}
+					}
+
+				} catch (JsonException x) {
+					throw x;
+				} catch (Exception x) {
+					throw x;
+				}
+			}
+
+			return tReturn;
+
+		}
+
+		public static async Task<T> RequestAsync (string URL, RequestType requestType = RequestType.Get, KeyValuePair<string,string>[] SendObject = null, int triesNumber = 0)
+		{
+
+			T tReturn = null;
+
+			for (int i = 0; i <= triesNumber; i++) {
+				try {
+
+					using (HttpClient client = new HttpClient ()) {
+
+						HttpResponseMessage response = new HttpResponseMessage ();
+
+						client.BaseAddress = new Uri (BaseUrl);
+
+						HttpContent httpContent = new FormUrlEncodedContent (SendObject);
+
+						switch (requestType) {
+						case RequestType.Get:
+							response = await client.GetAsync (URL);
+							break;
+						case RequestType.Post:
+							response = await client.PostAsync (URL, httpContent);
+							break;
+						case RequestType.Put:
+							response = await client.PutAsync (URL, httpContent);
+							break;
+						case RequestType.Delete:
+							response = await client.DeleteAsync (URL);
+							break;
+						default:
+							break;
+						}
+
+						if (response.IsSuccessStatusCode) {
+							string responseString = await response.Content.ReadAsStringAsync ();
+
+							var objetoRetorno = JsonHelper<T>.JsonToObject (responseString);
+
+
+							tReturn = objetoRetorno;
+
+						} else {
+							if (response.StatusCode == System.Net.HttpStatusCode.RequestTimeout) {
+								if (i == triesNumber)
+									throw  new Exception ("Tempo limite atingido");
+							} else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+								throw  new Exception ("Serviço não encontrado");
+							else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError) {
+								if (i == triesNumber)
+									throw new Exception ("Serviço indisponível no momento");
+							} else
+								throw new Exception ("Falha no acesso!");
+						}
+					}
+
+				} catch (JsonException x) {
+					throw x;
+				} catch (Exception x) {
+					throw x;
+				}
+			}
+
+			return tReturn;
+
+		}
+	}
+
+	public enum RequestType
+	{
+		Get,
+		Post,
+		Put,
+		Delete
+	}
+
+}
+
